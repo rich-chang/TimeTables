9e51e0cc66f4ddeeb232e26347e058ed2fb28a1c
Rich Chang <rich-chang@outlook.com>
2019-11-07 17:37:07 +0800

    init android project

---
+++ app/src/main/java/com/richc/timetables/MainActivity.java
@@ -0,0 +1,14 @@
+package com.richc.timetables;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
