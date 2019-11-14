1f243c0d738cacefb312c49d0eaf8ed1cb2dec0d
Rich Chang <rich_chang@outlook.com>
2019-11-14 23:28:43 +0800

    Add variable and seekbar listener

--- app/src/main/java/com/richc/timetables/MainActivity.java
+++ app/src/main/java/com/richc/timetables/MainActivity.java
@@ -3,6 +3,9 @@ package com.richc.timetables;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SeekBar timeTableSeekBar = findViewById(R.id.timeTableSeekBar);
+        ListView timeTableListView = findViewById(R.id.timeTableListView);
+
+        timeTableSeekBar.setMax(20);
+        timeTableSeekBar.setProgress(10);
+
+        timeTableSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                int min = 1;
+
+                if (i < min) {
+                    // Not allow zero as minimum
+                    timeTableSeekBar.setProgress(min);
+                } else {
+
+                }
+
+                Log.i("SeekBar value", Integer.toString(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
